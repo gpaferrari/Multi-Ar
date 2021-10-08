2a18649f580649d719bf822f52b50728cff01141
gpaferrari <79278499+gpaferrari@users.noreply.github.com>
2021-10-08 18:24:00 -0300

    PT10

--- _javaScripit/myScript.js
+++ _javaScripit/myScript.js
@@ -1,8 +1,38 @@
-function enviar{
+    // calculo de btus por m2
+    
+    function calcular (){
+        
+        var tamanho = document.getElementById("tamanho").value 
+        var nPessoas = document.getElementById("nPessoas").value
+        var eletronico = document.getElementById("eletronicos").value
+        var iSolar = document.getElementById("sol");
+        if(iSolar.value == "sim"){
+            iSolar = 3000
+        }else{
+            iSolar = 0
+        }
+        
+        
+            
+            
+        
+        var resultado = (tamanho * 750) + (nPessoas * 600) + (eletronico * 600) + iSolar
+
+        
+
+
+        return alert("Você precisa de um ar com capacidade de "+ resultado + " BTUh")
+    }
+
+
+
+
+
+
+
+
     //tratamento e-mail
 
-    var retorno = true
     
 
 
-}
